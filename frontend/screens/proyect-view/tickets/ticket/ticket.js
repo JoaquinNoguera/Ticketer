70bd40a97600b4939bbc574613634f01d8c5e9f7
Joaquin Noguera <nvjoaquin@gmail.com>
2020-01-25 18:27:55 -0300

    Tickets de proyecto dinamicos

--- frontend/screens/proyect-view/tickets/ticket/ticket.js
+++ frontend/screens/proyect-view/tickets/ticket/ticket.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
 import './styles.scss'
 
-const Ticket = function ({ name }) {
-    return (
-        <div className='proyect_view-tickets-ticket'> { name } </div>
-    );
+class Ticket extends React.Component {
+    
+
+    render(){
+        const {id, description, body} = this.props
+        return (
+            <>
+            <div className='proyect_view-tickets-ticket' onClick={this.changeShow}>
+                <h2> #{id} </h2>
+                <p> {description} </p>
+            </div>
+            </>
+        );
+    }
 }
 
 export default Ticket
