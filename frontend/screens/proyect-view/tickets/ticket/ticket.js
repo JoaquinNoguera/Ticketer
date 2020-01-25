978d3d902d4738c2de3f24ca4cfe046018468ffe
Joaquin Noguera <nvjoaquin@gmail.com>
2020-01-25 19:28:52 -0300

    se agrego el popup de los tickets

--- frontend/screens/proyect-view/tickets/ticket/ticket.js
+++ frontend/screens/proyect-view/tickets/ticket/ticket.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import './styles.scss'
+import PopupTicket from '../../popup-ticket'
 
 class Ticket extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            show: false,
+        }
+        this.onChangeShow = this.onChangeShow.bind(this);
+    }
     
+    onChangeShow(){
+        this.setState((state)=>({
+            show: !state.show,
+        }))
+    }
 
     render(){
-        const {id, description, body} = this.props
+        const {id, description, body} = this.props;
+        const {show} = this.state;
         return (
             <>
-            <div className='proyect_view-tickets-ticket' onClick={this.changeShow}>
+            <div className='proyect_view-tickets-ticket' onClick={this.onChangeShow}>
                 <h2> #{id} </h2>
                 <p> {description} </p>
+                <PopupTicket 
+                    id={id}
+                    show={show}
+                    body={body}
+                    description={description}
+                    onChangeShow = {this.onChangeShow}
+                />
             </div>
             </>
         );
