1c4ec0b6637d3f07cde518b5741a4471df4f530c
Jere <jere.chiosso@gmail.com>
2020-04-22 18:01:02 -0300

    implementado userdetails con jpa + refactoreos

--- backend/src/main/java/com/lambda/ticketer/users/UserDetailsService.java
+++ backend/src/main/java/com/lambda/ticketer/users/UserDetailsService.java
@@ -1,21 +1,28 @@
 package com.lambda.ticketer.users;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+
 @Service
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
+    @Autowired
+    private UsersRepository usersRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        if (s.equals("test")) {
-            return new User("test", "test", new ArrayList<>());
-        } else {
-            return null;
-        }
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = usersRepository.findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No se encontró al usuario "+ username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getName(),
+                user.getPasswordHash(),
+                new ArrayList<>()
+        );
     }
 }
